34b7a4a15b662f14bae5e838bb37cedf004c6a75
Mehul Rao <mehulrao@gmail.com>
2023-03-12 20:32:11 -0400

    update main

--- src/main.py
+++ src/main.py
@@ -6,19 +6,23 @@ total_count = 0
 
 send_sms("Starting script...")
 
-while get_available_rooms() <= 0:
-    print("Not available")
-    sleep(60)
-    if count == 1440:
-        message_fail = send_sms("No rooms available for the past 24 hours...")
-        print(message_fail)
-        count = 0
-    count += 1
+while True:
+    if get_available_rooms() <= 0:
+        print("Not available")
+        if count == 72:
+            message_fail_text = "No rooms available for the past 24 hours... Current availablity: " + \
+                str(get_available_rooms())
+            message_fail = send_sms(message_fail_text)
+            print(message_fail)
+            count = 0
+        count += 1
+    else:
+        message_text = "There are " + \
+            str(get_available_rooms()) + " rooms available!"
+        message_success = send_sms(message_text)
+        success_text = "SUCCESS!! " + "Total checks: " + str(total_count)
+        send_sms(success_text)
+        print(success_text)
+
     total_count += 1
-
-
-message_text = "There are " + str(get_available_rooms()) + " rooms available!"
-message_success = send_sms(message_text)
-success_text = "SUCCESS!! " + "Total checks: " + str(total_count)
-send_sms(success_text)
-print(success_text)
+    sleep(1200)
