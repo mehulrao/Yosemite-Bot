bfb22adc18077cf4a337b0a403429668c0ca37a1
Mehul Rao <mehulrao@gmail.com>
2023-04-10 17:31:18 -0400

    add sequoia

--- src/main.py
+++ src/main.py
@@ -1,28 +1,78 @@
 from methods import *
 from time import sleep
 
-count = 0
+yosemite_count = 0
+sequoia_count = 0
 total_count = 0
 
-send_sms("Starting script...")
+yosemite_rooms = get_yosemite_rooms()
+sequoia_rooms = get_sequoia_rooms()
+wuksachi_text = ""
+john_muir_text = ""
+if sequoia_rooms["wuksachi"] == True:
+    wuksachi_text = "Wuksachi is available"
+else:
+    wuksachi_text = "Wuksachi is not available"
+if sequoia_rooms["john_muir"] == True:
+    john_muir_text = "John Muir is available"
+else:
+    john_muir_text = "John Muir is not available"
+
+start_text = "Starting script... Number of Rooms in Yosemite: " + \
+    str(yosemite_rooms) + " " + wuksachi_text + " " + john_muir_text
+
+send_sms(start_text)
 
 while True:
-    if get_available_rooms() <= 0:
-        print("Not available")
+    yosemite_rooms = get_yosemite_rooms()
+    sequoia_rooms = get_sequoia_rooms()
+    # --- Yosemite ---
+    if yosemite_rooms <= 0:
+        print("Yosemite Not available")
         if count == 72:
-            message_fail_text = "No rooms available for the past 24 hours... Current availablity: " + \
-                str(get_available_rooms())
+            message_fail_text = "No Yosemite rooms available for the past 24 hours... Current availablity: " + \
+                str(yosemite_rooms)
             message_fail = send_sms(message_fail_text)
             print(message_fail)
             count = 0
         count += 1
     else:
         message_text = "There are " + \
-            str(get_available_rooms()) + " rooms available!"
+            str(yosemite_rooms) + " rooms available!"
         message_success = send_sms(message_text)
-        success_text = "SUCCESS!! " + "Total checks: " + str(total_count)
+        success_text = "YOSEMITE SUCCESS!! " + \
+            "Total checks: " + str(total_count)
         send_sms(success_text)
         print(success_text)
+        count = 0
+    # --- Sequoia ---
+    if sequoia_rooms["wuksachi"] == False and sequoia_rooms["john_muir"] == False:
+        print("Sequoia Not available")
+        if count == 72:
+            message_fail_text = "No Sequoia rooms available for the past 24 hours..."
+            message_fail = send_sms(message_fail_text)
+            print(message_fail)
+            count = 0
+    else:
+        message_text = ""
+        wuksachi_text = ""
+        john_muir_text = ""
+        if sequoia_rooms["wuksachi"] == True:
+            wuksachi_text = "Wuksachi is available"
+        else:
+            wuksachi_text = "Wuksachi is not available"
+        if sequoia_rooms["john_muir"] == True:
+            john_muir_text = "John Muir is available"
+        else:
+            john_muir_text = "John Muir is not available"
+        message_text = wuksachi_text + " and " + john_muir_text
+        message_text.strip()
+        message_success = send_sms(message_text)
+        success_text = "SEQUIOA SUCCESS!! " + \
+            "Total checks: " + str(total_count)
+        send_sms(success_text)
+        print(success_text)
+        count = 0
 
     total_count += 1
     sleep(1200)
--- src/methods.py
+++ src/methods.py
@@ -5,7 +5,7 @@ import re
 from twilio.rest import Client
 
 
-def get_available_rooms():
+def get_yosemite_rooms() -> int:
     url = "https://reservations.ahlsmsworld.com/Yosemite/Search/GetInventoryCountData"
 
     querystring = {"callback": "$.wxa.on_datepicker_general_availability_loaded", "CresPropCode": "000000",
@@ -30,6 +30,136 @@ def get_available_rooms():
     except:
         print("Error: " + text)
         available = 0
+
+    return available
+
+
+def get_sequoia_rooms() -> dict:
+    url = "https://services-p1.synxis.com/gw/product/v1/getLeadAvailability"
+    available = {}
+
+    wuksachi_payload = {
+        "Version": "1",
+        "Criterion": {
+            "listAllocationBlocks": True,
+            "NumRooms": 1,
+            "Currency": {"Code": "USD"},
+            "ChannelList": {
+                "PrimaryChannel": {"code": "WEB"},
+                "SecondaryChannel": {"code": "GC"}
+            },
+            "StartDate": "2023-06-11",
+            "EndDate": "2023-06-12",
+            "LengthOfStay": 1,
+            "LoyaltyList": [],
+            "onlyCheckRequested": False,
+            "AgentInfo": {},
+            "AccessCode": {},
+            "RoomStay": {
+                "GuestCount": [
+                    {
+                        "ageQualifyingCode": "Adult",
+                        "numGuests": 2
+                    },
+                    {
+                        "ageQualifyingCode": "Child",
+                        "numGuests": 0,
+                        "Ages": []
+                    }
+                ],
+                "RateList": [],
+                "RoomList": [],
+                "RateFilterList": []
+            }
+        },
+        "HotelList": [{"id": "404"}],
+        "UserDetails": {"Preferences": {"ResponseOptions": "ReturnAllocationBlocks"}},
+        "Chain": {"id": "398"}
+    }
+
+    wuksachi_headers = {
+        "cookie": "apisession=MDAxMjF-eHJIZVo2RldLTFQwTzJ6eDczbGEyaFBDeHMwTGU1SlhISnhDRUZaRnR0VXNhV09ESWhsTTNDaUtTS3pLSnR0K1NhK01PdWRneTdPb2UwWmJRZTNCajkyTGlIYXJCL2pEclpwZktMWHVxYmhvbVNNa2FkQ0xrMjdKSEFsOGxGNHdOOFozS0lDNnVXeFlsSW4vVnkwK1VxLzJNUHhJcWRHeitUV2wwUHhTOUswZWF2OGFzTVQxcnd5TjlDVFcvZUhUa0UxcUFiRzdJNnQ3QTArWW5ZMFM4U3kvS2Q0RjlmSWd6UFJNVG4rSXZkWT0; visid_incap_2695530=VTdbpQ%2BhTweMjOBH5rZpKjV1NGQAAAAAQUIPAAAAAABgxu1PmB%2BNaQF1uPwuHGIT; incap_ses_891_2695530=tvLSbgm%2Br2yDSl61pHhdDDV1NGQAAAAA5lVXEIuN8pNxS11Eq4LLiA%3D%3D",
+        "Accept": "application/json, text/plain, */*",
+        "Authorization": "ApiKey MDAxMjF+dVNKZlBxNkNpazBlb2Q5SkEwL0xmYU9DMFdSVzZxUW5sNnpCQkhNR3RhZVVVVkFRYzFOazdDQlhKa0xGRFZBTWVtUVFXdkFEcThkNjVoUVB2Wjd4dHc9PQ==",
+        "Content-Type": "application/json",
+        "Origin": "https://be.synxis.com",
+        "Referer": "https://be.synxis.com/",
+        "Sec-Fetch-Dest": "empty",
+        "Sec-Fetch-Mode": "cors",
+        "Sec-Fetch-Site": "same-site",
+        "activityid": "YpfHclIXpf"
+    }
+
+    wuksachi_response = requests.request(
+        "POST", url, json=wuksachi_payload, headers=wuksachi_headers)
+
+    try:
+        text = json.loads(wuksachi_response.text)
+        available["wuksachi"] = text["LeadAvailabilityList"][0]["Available"]
+    except:
+        print("Error: " + wuksachi_response.text)
+        available["wuksachi"] = False
+
+    john_muir_payload = {
+        "Version": "1",
+        "Criterion": {
+            "listAllocationBlocks": True,
+            "NumRooms": 1,
+            "Currency": {"Code": "USD"},
+            "ChannelList": {
+                "PrimaryChannel": {"code": "WEB"},
+                "SecondaryChannel": {"code": "GC"}
+            },
+            "StartDate": "2023-06-01",
+            "EndDate": "2023-07-31",
+            "LengthOfStay": 1,
+            "LoyaltyList": [],
+            "onlyCheckRequested": False,
+            "AgentInfo": {},
+            "AccessCode": {},
+            "RoomStay": {
+                "GuestCount": [
+                    {
+                        "ageQualifyingCode": "Adult",
+                        "numGuests": 2
+                    },
+                    {
+                        "ageQualifyingCode": "Child",
+                        "numGuests": 0,
+                        "Ages": []
+                    }
+                ],
+                "RateList": [],
+                "RoomList": [],
+                "RateFilterList": []
+            }
+        },
+        "HotelList": [{"id": "59989"}],
+        "UserDetails": {"Preferences": {"ResponseOptions": "ReturnAllocationBlocks"}},
+        "Chain": {"id": "398"}
+    }
+
+    john_muir_headers = {
+        "cookie": "apisession=MDAxMjF-RkNMTGY2RjFMUGViYkVWL1NwRzZCOTNBY25hQkUzR3V5UFlmSVhZcjVGaFVicDEzRzZBS20zeVUwSytTQ3JxUWVHTTdlTW9MUGpJRjhaVHZUSVVoKzhucGNWVWZQbVJqTXZCT1UxK2M2SHBkZTRveitzRUVRV09nTEozK3l4UDlLNy9jd2J2d05Qc0lERWVXTHUvNkFzVE81SUxKcjZRc3RmUWh4amN5SVBSVW9yUE1SOTFWaUVJUXN5MUc4S1ZKSjZGYURPeHBHZVYvMGZyaVNySGx1Qi94L2JRTGt5ZWNaOFJha0JlcmFWcz0; visid_incap_2695530=VTdbpQ%2BhTweMjOBH5rZpKjV1NGQAAAAAQUIPAAAAAABgxu1PmB%2BNaQF1uPwuHGIT; incap_ses_891_2695530=tvLSbgm%2Br2yDSl61pHhdDDV1NGQAAAAA5lVXEIuN8pNxS11Eq4LLiA%3D%3D",
+        "Accept": "application/json, text/plain, */*",
+        "Authorization": "ApiKey MDAxMjF+dVNKZlBxNkNpazBlb2Q5SkEwL0xmYU9DMFdSVzZxUW5sNnpCQkhNR3RhZVVVVkFRYzFOazdDQlhKa0xGRFZBTWVtUVFXdkFEcThkNjVoUVB2Wjd4dHc9PQ==",
+        "Content-Type": "application/json",
+        "Origin": "https://be.synxis.com",
+        "Referer": "https://be.synxis.com/",
+        "Sec-Fetch-Dest": "empty",
+        "Sec-Fetch-Mode": "cors",
+        "Sec-Fetch-Site": "same-site",
+        "activityid": "shsfClnAwQ"
+    }
+
+    john_muir_response = requests.request(
+        "POST", url, json=john_muir_payload, headers=john_muir_headers)
+    try:
+        text = json.loads(john_muir_response.text)
+        available["john_muir"] = text["LeadAvailabilityList"][0]["Available"]
+    except:
+        print("Error: " + john_muir_response.text)
+        available["john_muir"] = False
 
     return available
 
